ce015fbea4fe4f09a5e34260d20a81c976cd4563
ugackMiner53 <ugackMiner@gmail.com>
2024-11-12 22:01:53 -0500

    Support loading unpacked mods for faster development (Unpacked mods do not overwrite existing files)

--- loadot/core/loader.gd
+++ loadot/core/loader.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var logger = Loadot.Logger.new("Loadot")
-
 const MOD_DIR = "loadot/mods/"
+
+var logger = Loadot.Logger.new("Loadot")
+var unpacked_directories : PackedStringArray = []
 
 func _init():
     logger.debug("Starting Loadot Loader...")
@@ -18,7 +19,9 @@ func load_packed_mods(packed_directory : String):
     var mod_dir = DirAccess.open(packed_directory)
     if mod_dir:
         for directory in mod_dir.get_directories():
-            logger.debug("Folder loading currently unsupported. Skipping %s" % directory)            
+            directory = packed_directory.path_join(directory)
+            logger.debug("Loading unpacked directory %s" % directory)
+            unpacked_directories.append(directory) 
 
         for file in mod_dir.get_files():
             file = packed_directory.path_join(file)
@@ -33,22 +36,31 @@ func load_mods(mod_directory : String):
     if not mod_dir:
         logger.error("Mod directory %s not found!" % mod_directory)
         return
-    
-    var directories = mod_dir.get_directories()
 
     # Godot 4.4+ function to list folders in Resources instead of DirAccess (see https://github.com/godotengine/godot/pull/96590)
     # Commented out because it's unclear if FileAccess 
     # if ResourceLoader.has_method("list_directory"):
     #    directories = ResourceLoader.call("list_directory", mod_directory)
 
-    for directory in directories: 
-        logger.debug("Found directory %s, finding main and loading..." % directory)
-        var mod_main := mod_directory.path_join(directory).path_join("main.gd")
-        if ResourceLoader.exists(mod_main):
-            load(mod_main).new()
-            logger.debug("Loaded %s" % directory)
-        else:
-            logger.warning("Folder %s does not have main.gd! Skipping..." % directory)
+
+    # Load packed mods
+    for directory in mod_dir.get_directories(): 
+        logger.debug("Found mod %s, loading..." % directory)
+        load_mod(mod_directory.path_join(directory))
+    
+    # Load unpacked mods
+    for directory in unpacked_directories:
+        logger.debug("Found debug mod %s, loading..." % directory)
+        logger.warning("Unpacked mods do not overwrite builtin files. Pack the mod into a .zip to overwrite files.")
+        load_mod(directory)
+
+
+func load_mod(mod_path : String):
+    var mod_main := mod_path.path_join("main.gd")
+    if ResourceLoader.exists(mod_main):
+        load(mod_main).new()
+    else:
+        logger.warning("%s does not exist! Skipping..." % mod_main)
 
 
 func local_folder_dir(subfolder := "") -> String:
